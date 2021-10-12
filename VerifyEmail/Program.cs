993281ca2e74b0d0a653952580d2c764cc5d034a
MinhUet <buivanminh1309@gmail.com>
2021-10-12 14:17:23 +0700

    feat: Verify email

---
+++ VerifyEmail/Program.cs
@@ -0,0 +1,40 @@
+﻿using EASendMail;
+using System;
+
+namespace Google.Ads.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                SmtpMail oMail = new SmtpMail("TryIt");
+
+                // Set sender email address, please change it to yours
+                oMail.From = "dvhung.rac1@gmail.com";
+
+                // Set recipient email address, please change it to yours
+                oMail.To.Add(new MailAddress("dvhung.rac2@gmail.com"));
+                //oMail.To.Add(new MailAddress("lallalalalalal1111@gmail.com"));
+                //oMail.To.Add(new MailAddress("lallalalalalal222222@gmail.com"));
+
+                // Do not set SMTP server address
+                SmtpServer oServer = new SmtpServer("");
+
+                Console.WriteLine("start to test email address ...");
+
+                SmtpClient oSmtp = new SmtpClient();
+                oSmtp.TestRecipients(oServer, oMail);
+
+                Console.WriteLine("email address was verified!");
+            }
+            catch (SmtpServerException ep)
+            {
+                Console.WriteLine("failed to test email with the following error:");
+                Console.WriteLine(ep.Message);
+            }
+
+        }
+    }
+}
